5a61f5a2b45727c252c7ecad12ca486d7b65ed34
Rodrigo Bezerra Remígio de Queiroz <rodrigo.brq@gmail.com>
2019-03-26 15:14:53 +0000

    Trabalhando com variáveis

---
+++ 2.07 Variáveis/Variavel1.java
@@ -0,0 +1,13 @@
+public class Variavel1 {
+	
+	public static void main(String[] args) {
+		int quantidade; // Declaração de veriável
+		quantidade = 10; // Atribuindo o valor
+		System.out.println(quantidade);
+		
+		quantidade = 15; // Mudando o valor
+		
+		System.out.println(quantidade);
+	}
+	
+}
