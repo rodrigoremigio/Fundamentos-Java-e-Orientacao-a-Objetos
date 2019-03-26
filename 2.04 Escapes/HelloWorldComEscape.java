f2431bc1615565a17c60c44b9da466d94dad2006
Rodrigo Bezerra Remígio de Queiroz <rodrigo.brq@gmail.com>
2019-03-26 15:04:24 +0000

    Sequências de Escape

---
+++ 2.04 Escapes/HelloWorldComEscape.java
@@ -0,0 +1,10 @@
+public class HelloWorldComEscape {
+	
+	public static void main(String[] args) {
+		// A contra-barra faz as aspas duplas serem impressas na tela.
+		System.out.println("Hello, \"world.\"");
+		// Duas contra-barras faz uma ser impressa na tela.
+		System.out.println("Hello, \\world.\\");
+	}
+	
+}
