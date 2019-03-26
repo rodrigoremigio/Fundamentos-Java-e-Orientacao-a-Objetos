2fb28d4164af091ebba10535118f03ef60b8feec
Rodrigo Bezerra Remígio de Queiroz <rodrigo.brq@gmail.com>
2019-03-26 15:27:11 +0000

    Nomeando variáveis

---
+++ 2.08 Variáveis/Variavel2.java
@@ -0,0 +1,28 @@
+public class Variavel2 {
+	
+	public static void main(String[] args) {
+		int quantidade;
+		int quantidade_alunos;
+		int QUANTIDADE;
+		int QuantidadeAlunos;
+		int $quantidade;
+		int _quantidade;
+		int quantidade_alunos_nota_10;
+		
+		// Não pode começar com dígitos: int 20alunos;
+		// Não pode ter espaços: int quantidade alunos;
+		// Não pode ter palavra reservada: int new;
+		
+		// A conveção de código nos pede para escrever a primeira letra minúscula e a segunda palavra maiúscula.
+		
+		int quantidadeEstudantes;
+		int quantidadeEstudantesNota10;
+		int numeroAlunosAprovados;
+		
+		// Não abrevie demais a variável
+		
+		int qtdeAlu;
+		int quantAluApro;
+	}
+	
+}
