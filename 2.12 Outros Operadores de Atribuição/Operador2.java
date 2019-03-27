61dbd8a19925bc3556020114082354d0c06fbd6d
Rodrigo Bezerra Remígio de Queiroz <rodrigo.brq@gmail.com>
2019-03-27 15:22:36 +0000

    Outros operadores de atribuição

---
+++ 2.12 Outros Operadores de Atribuição/Operador2.java
@@ -0,0 +1,21 @@
+public class Operador2 {
+	
+	public static void main(String[] args) {
+		// Outros operadores de atribuição
+		int total = 10;
+		total += 3; // Soma o "total" + 3, sem precisar colocar total = total + 3.
+		System.out.println(total);
+		
+		total -= 2; // diminui 2 do valor de total.
+		System.out.println(total);
+		
+		total *= 3; // multiplica o valor de total por 3.
+		System.out.println(total);
+		
+		total /= 3; // divide o valor de total por 3.
+		System.out.println(total);
+		
+		total %= 5; // Exibe o resto de divisão de total por 5.
+		System.out.println(total);
+	}
+}
